8bb8b8005ed59db73243ee4aa00c186dc22629d4
MANOJ CHOUDHARY V <manojsasi20032005@gmail.com>
2024-07-10 20:18:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const[mydata,setMyData]=useState([]);
+  const [count,setCount]=useState(30);
+  useEffect(()=>{
+    axios.get(`https://rickandmortyapi.com/api/character?page=${count}`)
+    .then((res)=>setMyData(res.data.results))
+    .catch((error) => console.error("Error fetching data: ", error));
+    
+  },[count]);
 
+  const handleNext=()=>{
+    setCount(count+1);  
+    console.log(count);
+  }
+  const handlePrev=()=>{
+    setCount(count-1);  
+    console.log(count);
+  }
+
+
+return(
+  <>
+  <div className='App'>
+  
+  <div className='container'>
+  <h1>Rick and Morty</h1>
+
+    <div className='characters'>
+  
+    {
+      mydata?.map((character)=>{
+        // const {id,name,image,species,location}=character;
+        return(
+          <div className="card">
+          <img src={character.image} alt="" />
+          <div className="text-container">
+          <h3>{character.name}</h3>
+          <p className="status">
+          {character.status} - {character.species}
+          </p>
+          <p className="title">Last seen on</p>
+          <p>{character.location.name}</p>
+      </div>
+    </div>
+        )
+      })
+    }
+    </div>
+    <button onClick={handlePrev}>Prev</button>
+    <button onClick={handleNext}>Next</button>
+    </div>
+    </div>
+  </>
+)
+}
 export default App;
